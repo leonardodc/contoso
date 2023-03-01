2873a4f13ccd7b30a04a0eef711349a29d51fac7
Your Name <leonardo_foz@hotmail.com>
2023-03-01 15:40:57 -0300

    Unitest

--- readme.mk
+++ readme.mk
@@ -9,3 +9,17 @@
 	* métodos 
 	
 	* modelagem
+
+
+# Unittest
+
+###Convenções de nomenclatura
+#### Os nomes de classe e método estão seguindo uma convenção de teste. 
+#### A convenção é que eles precisam ser prefixados com test. 
+#### Embora não seja obrigatório, as classes de teste usam camel-casing, 
+#### e os métodos de teste são em letras minúsculas e as palavras são separadas por um sublinhado.
+
+* self.assertTrue(value): verifique se value é true.
+* self.assertFalse(value): verifique se value é false.
+* self.assertNotEqual(a, b): confirme que a e b não são iguais.
+* self.assertEqual(a, b): confirme que a e b são iguais.
