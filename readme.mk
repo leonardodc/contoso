d692d2ae3e8bce64ad50ec6cb84399db081dcf4e
Leonardo Demeterko <leonardo_foz@hotmail.com>
2023-02-28 23:57:10 -0300

    Test form Html

---
+++ readme.mk
@@ -0,0 +1,11 @@
+# Readme
+
+# Python and Flask - build AI web app
+
+## Note
+#### Ao criar um aplicativo Web com qualquer estrutura, há alguns conceitos principais que precisamos entender: 
+	* roteamento
+
+	* métodos 
+	
+	* modelagem
